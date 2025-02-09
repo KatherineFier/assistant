463e22733aa9621c1afa4e42355fb695158e4917
katherine_fier <krfier@gmail.com>
2025-02-09 11:47:45 -0700

    made assistant multi turn

--- assistant.py
+++ assistant.py
@@ -2,6 +2,21 @@ from openai import OpenAI
 import time
 client= OpenAI()
 
+def process_run(thread_id, assistant_id):
+    new_run = client.beta.threads.runs.create(
+    thread_id=thread_id,
+    assistant_id = assistant_id
+)
+
+    while True:
+        time.sleep(1)
+        run_check = client.beta.threads.runs.retrieve(
+            thread_id=thread_id,
+            run_id=new_run.id
+        )
+        if run_check.status in ["cancelled", "failed", "completed", "expired"]:
+            return run_check
+
 
 assistant = client.beta.assistants.create(
     name = "Study Buddy",
@@ -12,33 +27,35 @@ assistant = client.beta.assistants.create(
 
 thread = client.beta.threads.create()
 
-user_input = input("Hello! I'm here to help. Please type your message.")
-
-message = client.beta.threads.messages.create(
-    thread_id = thread.id,
-    role = "user",
-    content = user_input
-)
-
-run = client.beta.threads.runs.create(
-    thread_id = thread.id,
-    assistant_id = assistant.id
-)
-
 while True:
-    time.sleep(1)
-    run = client.beta.threads.runs.retrieve(
-        thread_id = thread.id,
-        run_id = run.id
+    user_input = input("You: ")
+
+    if user_input.lower() == "exit":
+        exit()
+
+    message = client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=user_input
     )
+
+    run = process_run(thread.id, assistant.id)
+
     if run.status == "completed":
-        break
+        thread_messages = client.beta.threads.messages.list(
+            thread_id=thread.id
+        )
+
+        print("\nAssistant: " + thread_messages.data[0].content[0].text.value + "\n")
+
+    if run.status in ["cancelled", "failed", "expired"]:
+        print("\nAssistant: An error has occurred, please try again.\n")
 
 
-if run.status == "completed":
-    thread_messages = client.beta.threads.messages.list(
-        thread_id = thread.id
-    )
-    message_for_user = thread_messages.data[0].content[0].text.value
-    print(message_for_user)
+    # if run_check.status == "completed":
+    #     thread_messages = client.beta.threads.messages.list(
+    #         thread_id = thread.id
+    #     )
+    #     message_for_user = thread_messages.data[0].content[0].text.value
+    #     print(message_for_user)
 
